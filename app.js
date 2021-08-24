2ebcfecdf21c44839c8d583f2cfca6fb62c33d0a
edwinsenjaya <edwinsenjaya5@gmail.com>
2021-08-24 10:32:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+const router = require("./routes");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App working on port ${PORT}`);
+});
